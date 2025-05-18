8968df02d969dced631759420a6d539e32d91d12
Raj Verma <rajv23854@gmail.com>
2025-05-18 22:56:35 +0530

    using array

---
+++ Stack/implementation.cpp
@@ -0,0 +1,68 @@
+//using array to create stack.
+#include<iostream>
+using namespace std;
+class stack{
+     int *arr;
+     int size;
+     int top;
+     public:
+     stack(int s){
+         size=s;
+         top=-1;
+         arr=new int[s];
+     }
+
+     //push
+     void push(int value){
+        if(top==size-1){
+            cout<<"stack overflow/n";
+            return;
+        }
+        else{
+            top++;
+            arr[top]=value;
+            cout<<"pushed "<<value<<" into the stack\n";
+        }
+     }
+
+     //pop
+     void pop(){
+        if(top==-1){
+            cout<<"stack underflow/n";
+            return;
+        }
+        else{
+            top--;
+            cout<<"popped "<<arr[top+1]<<" from the stack\n";
+        }
+     }
+
+     //peek
+     int peek(){
+        if(top==-1){
+            cout<<"stack is empty/n";
+            return -1;
+        }
+        else{
+            return arr[top];
+        }
+     }
+
+     //is empty
+     bool isEmpty(){
+        return top==-1;
+     }
+
+     //is full
+     bool isFull(){
+        if(top=size-1)
+        return 1;
+        else 
+        return 0;
+     }
+
+     //is size
+     int isSize(){
+        return top+1;
+     }
+};
